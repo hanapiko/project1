2d06f10c60aa2288f6668cbec3e11ad332a55ee5
hanapiko <apikojuma94@gmail.com>
2024-07-18 17:25:22 +0300

    sort

--- myextras/sortwordarr/main.go
+++ myextras/sortwordarr/main.go
@@ -19,27 +19,32 @@
 package main
 
 import (
+	//"fmt"
+
 	"fmt"
-	"strings"
+
+	//"github.com/01-edu/z01"
+
+
 )
 
-func SortWordArr(a []string) {
-	n := len(a)
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-i-1; j++ {
-			if strings.Compare(a[j], a[j+1]) > 0 {
-				// Swap elements if they are in the wrong order
-				a[j], a[j+1] = a[j+1], a[j]
-			}
-		}
-	}
-}
 
 func main() {
 	result := []string{"a", "A", "1", "b", "B", "2", "c", "C", "3"}
 	SortWordArr(result)
-
 	fmt.Println(result)
+	
 }
+func SortWordArr(a []string) {
+	for i:=0; i < len(a); i++ {
+		for j:=i+1; j < len(a); j++ {
+			if a[i] > a[j]{
+				a[i],a[j] = a[j],a[i]
+			}
+		}
+	}
+	// fmt.Println(a)
+}
+
 
 
