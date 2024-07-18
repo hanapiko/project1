d71b92bbe0b74a1a5d002d0fe1debb46f77d928f
hanapiko <apikojuma94@gmail.com>
2024-07-18 14:13:49 +0300

    hex

--- myextras/printhex/main.go
+++ myextras/printhex/main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strconv"
+	"fmt"
 )
 
 func main() {
 	if len(os.Args) != 2 {
 		return
 	}
-	args := os.Args[1]
-	number, err := strconv.Atoi(args)
-	if err != nil || number < 0 {
-		fmt.Println("ERROR")
-		return
+
+	str := os.Args[1]
+	hex := "0123456789abcdef"
+	result := ""
+	num := 0
+	for _, ch := range str {
+		if ch >= '0' && ch <= '9' {
+			num = num*10+ int(ch -'0')
+		}	
 	}
-	base := strconv.FormatInt(int64(number), 16)
-	fmt.Println(base)
+	//num, _ := strconv.Atoi(str)
+
+	for num > 0 {
+		mod := num % 16
+		result = string(hex[mod]) + result
+		num /= 16
+	}
+
+	fmt.Println(result)
+
+
+	// args := os.Args[1]
+	// number, err := strconv.Atoi(args)
+	// if err != nil || number < 0 {
+	// 	fmt.Println("ERROR")
+	// 	return
+	// }
+	// base := strconv.FormatInt(int64(number), 16)
+	// fmt.Println(base)
 }
