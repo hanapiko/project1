8f163d1537bd272527867a4c31f1f7a661e5e9c5
hanapiko <apikojuma94@gmail.com>
2024-10-26 15:31:01 +0300

    gcd

--- myextras/gcd/main.go
+++ myextras/gcd/main.go
@@ -15,3 +15,21 @@ func Gcd(a, b int) int {
 	}
 	return a
 }
+
+// method2
+// if a == 0 || b == 0 {
+	// 	return 0
+	// }
+	// var num uint
+	// if a < b {
+	// 	num = a
+	// } else {
+	// 	num = b
+	// }
+
+	// for i := num; i > 0; i-- {
+	// 	if a % i == 0 && b % i == 0 {
+	// 		return i
+	// 	}
+	// }
+	// return 0
